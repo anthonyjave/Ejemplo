e3aa578557734a03e1094748e1e3239317bf44c7
anthonyjave <javerodriguezanthony@gmail.com>
2021-04-30 16:55:30 -0500

    version 1

---
+++ Prog1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[] { 1, 5, 112, 3, 7 };
+            int m = mayorElemento(x);
+
+            Console.WriteLine(m);
+            Console.Read();
+        }
+
+        private static int mayorElemento(int[] x)
+        {
+            int max = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (x[i] > max)
+                    max = x[i];
+            }
+            return max;
+        }
+    }
+}
